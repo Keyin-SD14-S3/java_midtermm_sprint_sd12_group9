5b7ee728fb3e904aec3d7979a90eca97271e3a70
Stephen Badcock <badcockstephen73@gmail.com>
2025-02-18 16:14:07 -0330

    giving getters to person super class

--- src/models/Person.java
+++ src/models/Person.java
@@ -1,11 +1,26 @@
 package src.models;
 
+/**
+ * Represents a generic person in the pharmacy system.
+ * This class serves as the base for Doctor and Patient.
+ * 
+ * @author Stephen
+ * @version 1.0
+ */
 public class Person {
-    protected String id;
-    protected String name;
-    protected int age;
-    protected String phoneNumber;
+    private String id;
+    private String name;
+    private int age;
+    private String phoneNumber;
 
+    /**
+     * Constructs a Person object.
+     * 
+     * @param id          The unique identifier for the person.
+     * @param name        The person's name.
+     * @param age         The person's age.
+     * @param phoneNumber The person's contact number.
+     */
     public Person(String id, String name, int age, String phoneNumber) {
         this.id = id;
         this.name = name;
@@ -13,12 +28,19 @@ public class Person {
         this.phoneNumber = phoneNumber;
     }
 
-        /**
-     * 
-    * 
-    * @author Stephen
-    * @version 1.0
-    */
+    /** Gets the ID of the person. */
+    public String getId() { return id; }
+
+    /** Gets the name of the person. */
+    public String getName() { return name; }
+
+    /** Gets the age of the person. */
+    public int getAge() { return age; }
+
+    /** Gets the phone number of the person. */
+    public String getPhoneNumber() { return phoneNumber; }
+}
+
 
     // Getters & Setters (To be implemented)
-}
+
